0c0e4316214390261e19e1e5ca5567eafb051878
YooKyungJung <yugyungs200@naver.com>
2017-09-19 20:06:31 +0900

    파이어 베이스 연동 연습

--- app/src/main/java/kr/hs/emirim/dbrud313/firebasedbexam/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbrud313/firebasedbexam/MainActivity.java
@@ -1,13 +1,24 @@
 package kr.hs.emirim.dbrud313.firebasedbexam;
 
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mNameTextView;
+    private TextView mGithubTextView;
+    private ImageView mProfilImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameTextView=(TextView) findViewById(R.id.profile_name);
+        mGithubTextView=(TextView) findViewById(R.id.profile_github);
+        mProfilImageView=(ImageView) findViewById(R.id.profile_image);
     }
 }
