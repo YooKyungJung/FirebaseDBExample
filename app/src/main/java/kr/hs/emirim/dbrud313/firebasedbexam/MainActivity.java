4ba5ce73e99e58a707d23e00fd517f2b371bc94e
YooKyungJung <yugyungs200@naver.com>
2017-09-19 20:29:49 +0900

    6.5.4 Picasso 라이브러리를 활용하여 테스트 웹 이미지 출력

--- app/src/main/java/kr/hs/emirim/dbrud313/firebasedbexam/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbrud313/firebasedbexam/MainActivity.java
@@ -1,16 +1,18 @@
 package kr.hs.emirim.dbrud313.firebasedbexam;
 
-
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+//import kr.hs.emirim.dbrud313.firebasedbexam.R;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mNameTextView;
+    private TextView mNameTextView; //멤버변수 이름 앞엔 m붙여서 보기 편하도록
     private TextView mGithubTextView;
-    private ImageView mProfilImageView;
+    private ImageView mProfileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         mNameTextView=(TextView) findViewById(R.id.profile_name);
         mGithubTextView=(TextView) findViewById(R.id.profile_github);
-        mProfilImageView=(ImageView) findViewById(R.id.profile_image);
+        mProfileImageView=(ImageView) findViewById(R.id.profile_image);
+
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(mProfileImageView); //context->흐름, 문맥
+
     }
 }
